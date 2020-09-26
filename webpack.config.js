aa122b5c0daa1a0e98151ec4410289bba4b5a9a2
a.savanovich <asavanster@gmail.com>
2020-09-26 13:12:03 +0300

    add manifest

--- webpack.config.js
+++ webpack.config.js
@@ -93,6 +93,7 @@ module.exports = (env, argv) => {
                 patterns: [
                     { from: 'src/images', to: './images' },
                     { from: 'src/rules.html', to: './' },
+                    { from: 'src/manifest.json', to: './' },
                     { from: 'github', to: './' }
                 ],
             })
