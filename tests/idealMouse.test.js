9cff643f7db5838d3642c150b7110f3e243bd409
a.savanovich <asavanster@gmail.com>
2020-09-30 07:11:45 +0300

    beautify tests

--- tests/idealMouse.test.js
+++ tests/idealMouse.test.js
@@ -6,54 +6,51 @@ function testFunc(t, hits, expected) {
     for (const h of hits) {
         mouse.hit(h - 1);
     }
-    t.deepEqual(mouse.getPositions(), expected);
+    t.deepEqual(mouse.getPositions().map(i => i+1).sort(), expected);
 }
-
 
 test('one position left', (t) => {
     const hits = [2, 3, 4, 2, 3];
-    testFunc(t, hits, [3]);
+    testFunc(t, hits, [4]);
 });
 
 test('two hits', (t) => {
     const hits = [2, 2];
-    testFunc(t, hits, [2,4,1,3]);
+    testFunc(t, hits, [2,3,4,5]);
 });
 
 test('four hits', (t) => {
     const hits = [2, 2, 3, 3];
-    testFunc(t, hits, [2,4,1,3]);
+    testFunc(t, hits, [2,3,4,5]);
 });
 
-test('mouse not dead', (t) => {
+test('mouse not dead1', (t) => {
     const hits = [2,2,4,4,4,3,3,2];
-    testFunc(t, hits, [2, 4, 1, 3]);
+    testFunc(t, hits, [2,3,4,5]);
 });
 
 test('mouse not dead2', (t) => {
     const hits = [2,2,4,4,4,2];
-    testFunc(t, hits, [2, 4, 1, 3]);
+    testFunc(t, hits, [2,3,4,5]);
 });
-
-test('mouse dead', (t) => {
-    const hits = [2,3,4,4,2,4,3,2];
-    testFunc(t, hits, []);
-});
-
 
 test('mouse not dead4', (t) => {
     const hits = [4,4,2,2,2,3,3,4];
-    testFunc(t, hits, [0, 2, 1, 3]);
+    testFunc(t, hits, [1, 2, 3, 4]);
 });
 
+// dead
 test('mouse dead1', (t) => {
     const hits = [2,3,4,2,3,4];
     testFunc(t, hits, []);
 });
-//
-//
-//
+
 test('mouse dead2', (t) => {
     const hits = [2, 3, 4, 4, 3, 2];
+    testFunc(t, hits, []);
+});
+
+test('mouse dead3', (t) => {
+    const hits = [2,3,4,4,2,4,3,2];
     testFunc(t, hits, []);
 });
